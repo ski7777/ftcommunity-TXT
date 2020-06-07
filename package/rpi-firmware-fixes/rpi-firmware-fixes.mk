027ae273afdcce79203283889e8a8a2d5bb4bbc5
Raphael Jacob <r.jacob2002@gmail.com>
2020-06-07 10:05:08 +0200

    rpi-firmware-fixes: Add disable_warnings option

--- package/rpi-firmware-fixes/rpi-firmware-fixes.mk
+++ package/rpi-firmware-fixes/rpi-firmware-fixes.mk
@@ -8,10 +8,18 @@ ifeq ($(BR2_PACKAGE_RPI_FIRMWARE_FIXES), y)
 	RPI_FIRMWARE_FIXES_DEPENDENCIES += rpi-bt-firmware
 endif
 
+BR2_PACKAGE_RPI_FIRMWARE_FIXES_DISABLE_WARNINGS_LEVEL = 1
+ifeq ($(BR2_PACKAGE_RPI_FIRMWARE_FIXES_DISABLE_WARNINGS_TURBO), y)
+	BR2_PACKAGE_RPI_FIRMWARE_FIXES_DISABLE_WARNINGS_LEVEL = 2
+endif
+
 define RPI_FIRMWARE_FIXES_BUILD_CMDS
 	$(INSTALL) package/rpi-firmware/config.txt $(@D)
 	if [ "$(BR2_PACKAGE_RPI_FIRMWARE_FIXES)" = "y" ]; then \
 		echo -e "\n# fixes rpi (3B, 3B+, 3A+, 4B and Zero W) ttyAMA0 serial console\ndtoverlay=miniuart-bt" >> $(@D)/config.txt; \
+	fi
+	if [ "$(BR2_PACKAGE_RPI_FIRMWARE_FIXES_DISABLE_WARNINGS)" = "y" ]; then \
+		echo -e "\n# disable low power warnings\navoid_warnings=$(BR2_PACKAGE_RPI_FIRMWARE_FIXES_DISABLE_WARNINGS_LEVEL)" >> $(@D)/config.txt; \
 	fi
 endef
 
